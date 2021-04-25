24ac077d5ba6b44ff8397e8c83004f333b36157d
diallomld <diallomld5@gmail.com>
2021-04-25 07:47:00 +0100

    design login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,74 +1,66 @@
-import React from "react";
-import { Button, makeStyles, TextField } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 import { useHistory } from "react-router-dom";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-
 import CircularProgress from "@material-ui/core/CircularProgress";
-
 import MainHeader from "../components/header/MainHeader";
 import { useGlobalContext } from "../context/context";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        Appflux
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
 const useStyles = makeStyles((theme) => ({
-  conatain: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
+  paper: {
+    marginTop: theme.spacing(16),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-  header: {
-    width: "100vw",
-    height: "70px",
-    backgroundColor: "#212F3C",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  formContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    marginBottom: 200,
-    marginTop: 150,
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    display: "flex",
-    flexDirection: "column",
-    width: "20%",
-    height: "40vh",
-    justifyContent: "center",
-    alignItems: "center",
-
-    padding: 10,
+    width: '72%',
+    marginTop: 8+'px',
   },
   submit: {
-    height: 40,
-    width: 250,
-    marginTop: 15,
+    margin: theme.spacing(2, 6, 2),
+    
     background: "#18A4F6",
     color: "white",
-    fontSize: 20,
+    fontSize:20
   },
   circle: {
     alignSelf: "center",
-    marginLeft: 100,
-  },
-  acc: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  icon: {
-    color: "white",
-  },
-  h: {
-    color: "white",
-    fontSize: 18,
+    marginLeft: 120,
   },
 }));
-const Login = ({ navigation }) => {
+
+export default function Login() {
+
   const { signIn, load, draw, setDrawer, err } = useGlobalContext();
   const initialState = {
     email: "",
@@ -104,52 +96,50 @@ const Login = ({ navigation }) => {
     text = "Revoyez votre email ";
     textt = "Revoyez votre mot de passe";
   }
+
   return (
     <>
-      <MainHeader />
-      <div className={classes.contain}>
-        {/* <div className={classes.header}>
-          <div onClick={handleClick} className={classes.acc}>
-            <ArrowBackIcon className={classes.icon} />
-            <h1 className={classes.h}>Acceuil</h1>
-          </div>
-        </div> */}
-
-        <div className={classes.formContainer}>
-          <form className={classes.form} noValidate onSubmit={handleSubmit}>
-            <TextField
-              error={errorr}
-              variant="outlined"
-              margin="normal"
-              required
-              //   fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={handleChange}
-              value={credentital.email}
-              helperText={text}
-              style={{ width: 200, borderRadius: 20 + 'px' }}
-            />
-            <TextField
-              error={errorr}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={handleChange}
-              value={credentital.password}
-              helperText={textt}
-              style={{ width: 200 }}
-            />
-
+    <MainHeader />
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Se connecter
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            error={errorr}
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={handleChange}
+            value={credentital.email}
+            helperText={text}
+          />
+          <TextField
+            variant="outlined"
+            error={errorr}
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={handleChange}
+            value={credentital.password}
+            helperText={textt}
+          />
             {load ? (
               <>
                 <Button
@@ -175,14 +165,19 @@ const Login = ({ navigation }) => {
                 Se connecter
               </Button>
             )}
-          </form>
-          <Link to="/inscription">
-            {"Vous n'avez pas de compte? S'enregistrer"}
-          </Link>
-        </div>
+          <Grid container>
+            <Grid item style={{marginTop:10, marginLeft:20, alignContent:'center'}}>
+              <Link  href="/inscription" variant="body2">
+                Vous n'avez pas de compte? S'inscrire
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
       </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
     </>
   );
-};
-
-export default Login;
+}
